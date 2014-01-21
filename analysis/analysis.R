761e599d952304b558fc44479e70729f8a9ae384
Aaron Steven White <aswhite@umd.edu>
2014-01-21 11:25:41 -0500

    added accuracy filtering (default threshold .9) and code for balancing the number of times a sentence was seen in the dataset

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -5,7 +5,7 @@ require(lmerTest)
 require(ggplot2)
 
 ## load data
-data <- read.csv("~/experiments/ComplementCoercion/data.csv")
+data <- read.csv("~/experiments/ComplementCoercion/data/data.csv")
 
 ## remove practice items
 data <- subset(data, type!='practice')
@@ -14,7 +14,7 @@ data <- subset(data, type!='practice')
 data$type <- data$type[drop=T,]
 
 ## move sentence number to column called sentence
-data$sentence <- data$item
+data$sentence.num <- data$item
 
 ## make item numbers
 data$item <- ceiling((data$item - 8) / 3)
@@ -47,7 +47,35 @@ for (part in reject.participant){
 #### drop rejected participants
 data$participant <- data$participant[drop=T,]
 
+## balance number of times a sentence was seen
+
+#### get sentences each participant saw
+part.sent.pairs <- count(data, c('participant', 'sentence.num'))[c('participant', 'sentence.num')]
+
+#### get count of how many times a sentence was seen by a participant
+sent.counts <- count(part.sent.pairs, 'sentence.num')
+
+#### merge the sent counts with the participant-sentence pairs
+part.sent.counts <- merge(part.sent.pairs, sent.counts, by='sentence.num')
+
+#### find those counts greater than the minimum
+min.count <- min(part.sent.counts$freq)
+part.sent.counts.gtmin <- subset(part.sent.counts, freq > min.count) 
+
+if (nrow(data) > 0){ # if there are sentences seen more than the minimum
+  unique.vals <- unique(part.sent.counts.gtmin$freq) # find the different counts greater than the min
+  for (i in unique.vals){ # for each of those
+    offending.parts <- unique(subset(part.sent.counts.gtmin, freq == i)$participant) # find out which participants saw them
+    j <- i - min.count # then find out how many participants you'll need to take out to get the sentence counts balanced
+    while (j > 0){
+      data <- subset(data, participant != offending.parts[length(offending.parts)-j]) # take out the last j participants run on that list
+      j <- j - 1
+    }
+  }
+}
+
 ## remove questions and positions above 6
+## these are unnecessary since the shortest sentence had 6 regions
 spr <- subset(data, position < 7 & !is.na(position))
 
 ## make position a factor
@@ -57,7 +85,7 @@ spr$position <- as.factor(spr$position)
 spr$inv.rt <- 1 / spr$rt
 spr$log.rt <- log(spr$rt)
 
-## two factor ANOVA with repeated measures on both factors
+## fit two factor ANOVA with repeated measures on both factors
 
 #### by-subject
 m.raw.aov.part <- aov(rt ~ type * position + Error(participant/(type * position)), data=spr)
